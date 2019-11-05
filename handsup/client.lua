7eb85b9684185d63004313ad32a5a93ebda02d49
yonaper1 <57345110+yonaper1@users.noreply.github.com>
2019-11-05 11:43:35 +0200

    Update client.lua

--- handsup/client.lua
+++ handsup/client.lua
@@ -1,19 +1,19 @@
 Citizen.CreateThread(function()
-    local dict = "missminuteman_1ig_2"
+    local dict = "world_human_cop_idles"
     
 	RequestAnimDict(dict)
 	while not HasAnimDictLoaded(dict) do
 		Citizen.Wait(100)
 	end
-    local handsup = false
+    local cop = false
 	while true do
 		Citizen.Wait(0)
 		if IsControlJustPressed(1, 323) then --Start holding X
-            if not handsup then
-                TaskPlayAnim(GetPlayerPed(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
-                handsup = true
+            if not cop then
+                TaskPlayAnim(GetPlayerPed(-1), dict, "cop_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
+                cop = true
             else
-                handsup = false
+                cop = false
                 ClearPedTasks(GetPlayerPed(-1))
             end
         end
